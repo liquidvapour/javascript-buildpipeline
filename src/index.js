0f34658e69519a77359743d15577bbd4afd7ce44
liquidvapour <liquidvapour@gmail.com>
2018-05-11 16:00:30 +0100

    Render ten divs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello, world");
+window.onload = function(e) {
+    var elem = document.createElement("div");
+    document.body.appendChild(elem);
+    var i;
+    for (i = 0; i < 10; i++) {
+        var child = document.createElement("div");
+        child.innerText = "foo: " + i;
+        elem.appendChild(child);
+    }
+};
